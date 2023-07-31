5038d4b1ddd9789e20a75780bd2baf7bace299f5
Maryam Ayemlo Gambo <82781910+MaryamGambo@users.noreply.github.com>
2023-07-31 13:47:21 -0500

    modified the validation from order_date to date

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :books, through: :order_items
 
-  validates :order_date, presence: true
+  validates :date, presence: true
   validates :gst, :hst, :pst, :total_amount, numericality: { greater_than_or_equal_to: 0 }
   validates :status, inclusion: { in: %w[new paid shipped] }
 
