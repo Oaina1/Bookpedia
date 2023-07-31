a6508cf2b441b733cc83d44bce3140fa426bf86c
Maryam Ayemlo Gambo <82781910+MaryamGambo@users.noreply.github.com>
2023-07-31 14:01:45 -0500

    registered model to active admin

---
+++ .history/app/admin/orders_20230731140115.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Order do
+  config.filters = false
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+   permit_params :date, :total_amount, :gst, :pst, :hst, :status, :customer_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:date, :total_amount, :gst, :pst, :hst, :status, :customer_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+end
