b9896fbb11d2f497f3eefe1f9ec0c881632f0b46
Maryam Ayemlo Gambo <82781910+MaryamGambo@users.noreply.github.com>
2023-07-30 22:19:48 -0500

    moved the active-admin javascript and stylesheet files to the vendor folder

--- .history/config/application_20230730183902.rb
+++ .history/config/application_20230730183902.rb
@@ -1,8 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-require 'active_admin'
-
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
